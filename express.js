2c21c7d391f8181d1d00072f5850565dd85d5438
Jack Penn <jacktpenn@gmail.com>
2025-01-26 00:37:02 -0500

    fix

--- express.js
+++ express.js
@@ -59,5 +59,5 @@ io.on("connection", (socket) => {
 });
 
 httpsServer.listen(port, () => {
-  console.log(`> Server running at http://${hostname}:${port}`);
+  console.log(`> Server running at https://${hostname}:${port}`);
 });
